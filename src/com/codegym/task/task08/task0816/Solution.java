431c5b4a446d887ec0ef61bc26573c8743459b09
sergiudragoi <Elvetia30>
2019-08-18 13:19:50 +0300

    15 aug Commit

--- src/com/codegym/task/task08/task0816/Solution.java
+++ src/com/codegym/task/task08/task0816/Solution.java
@@ -3,12 +3,14 @@ package com.codegym.task.task08.task0816;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
+import java.util.*;
+
+import static java.util.Calendar.AUGUST;
+import static java.util.Calendar.JUNE;
 
 /* 
 Kind Emma and the summer holidays
+summer lasts from June 1 to August 31.
 
 */
 
@@ -17,16 +19,40 @@ public class Solution {
         DateFormat df = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
         HashMap<String, Date> map = new HashMap<String, Date>();
         map.put("Stallone", df.parse("JUNE 1 1980"));
+        map.put("Sergiu", df.parse("JUNE 1 1990"));
+        map.put("Andrei", df.parse("AUGUST 5 1983"));
+        map.put("Radu", df.parse("MAY 6 1986"));
+        map.put("Vio", df.parse("JULY 7 1988"));
+        map.put("Dan", df.parse("OCTOBER 8 1976"));
+        map.put("Fane", df.parse("SEPTEMBER 9 1982"));
+        map.put("George", df.parse("JANUARY 12 1999"));
+        map.put("Cristi", df.parse("FEBRUARY 13 2003"));
+        map.put("Marius", df.parse("JUNE 16 1986"));
 
         //write your code here
+        return map;
     }
 
     public static void removeAllSummerPeople(HashMap<String, Date> map) {
         //write your code here
+        Calendar c = Calendar.getInstance();
+        Iterator<Map.Entry<String,Date>> it = map.entrySet().iterator();
+        while (it.hasNext()){
+            Map.Entry<String,Date> entry = it.next();
+            c.setTime(entry.getValue());
+            if (c.get(Calendar.MONTH) == 5 || c.get(Calendar.MONTH) == 6 || c.get(Calendar.MONTH )== 7){
+                it.remove();
+            }
+        }
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+
+        HashMap<String, Date> map2 = new HashMap<String, Date>();
+
+        map2 = createMap();
+        removeAllSummerPeople(map2);
 
     }
 }
